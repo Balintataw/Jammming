3a057154a3b3f7759321da1e33406dfdec4e2f9f
Balintataw <Balintataw2@gmail.com>
2017-10-09 08:36:21 -0500

    working build

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -10,7 +10,7 @@ class PlayList extends React.Component {
   }
 
   handleNameChange(event) {
-    this.props.onNameChange(event.target.value)
+    this.props.onNameChange(event.target.value);
   }
 
   render() {
